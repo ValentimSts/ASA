97469dc7c97ba648f3c23ebb7c35a080b89c6de0
Valentim Santos <75221226+ValentimSts@users.noreply.github.com>
2022-01-22 00:13:24 +0000

    Update p2.cpp

--- p2/p2.cpp
+++ p2/p2.cpp
@@ -43,19 +43,8 @@ void computeInput() {
     int u, v;
     // Graph used to represent the tree (transpose graph). Where each
     // index is used as the identifier of the node, and contains its
-    // parent id. (Ex: 1 -> 2, graph[1 (2-1)] = 1).
+    // parent(s) id, aswell as some extra information (explained bellow).
     int** graph = new int*[n_vertices];
-
-    // Array used to store the number of parents eah node has, if the
-    // node with id = 2 has 2 parents, n_parents[1 (2-1)] = 2. 
-    int** n_parents = new int*[n_vertices];
-
-    // Initializes the array with 0, as in the beggining we don't know
-    // how many parents each node has.
-    int init_val = 0;
-    for (int i = 0; i < n_vertices; i++) {
-        n_parents[i] = &init_val;
-    }
 
     for (int i = 0; i < n_edges; i++) {
         if (scanf("%d %d", &u, &v) != 2) {
@@ -63,30 +52,46 @@ void computeInput() {
             return;   
         }
 
-        std::cout << u << " " << v << std::endl;
         
-        graph[v-1] = &u;
-        std::cout << "joao?" << std::endl;
+        // v-1 because the node ids go from 1 to n
+        if (graph[v-1] == NULL) {
+            // If the node hasn't been initialized, we initialize it with
+            // 4 positions:
+            // ----------------------------------------------------------
+            //   index 0: first parent node
+            //   index 1: second parent node (if needed)
+            //   index 2: number of parents
+            //   index 3: weight of the node (to be found after the DFS)
+            graph[v-1] = new int[4];
 
-        // Increment the number of parents the node with id v-1 has.
-        n_parents[v-1] += 1;
-        std::cout << "joao2?" << std::endl;
+            graph[v-1][0] = -1;
+            graph[v-1][1] = -1;
+            graph[v-1][2] = 0;
+            graph[v-1][3] = 0;
+        }
+
+        int parent_pos = graph[v-1][2];
 
         // If the node has more than 2 parents, the tree is invalid.
-        if (*n_parents[v-1] > 2) {
-            std::cout << "-";
+        if (parent_pos == 2) {
+            std::cout << "-" << std::endl;
             return;
         }
-    }
 
-    std::cout << "hmmmmm" << std::endl;
+        graph[v-1][parent_pos] = u;
+
+        // Increments the number of parents of the node
+        graph[v-1][2]++;
+    }
 
     printGraph(graph, n_vertices);
 }
 
+void 
 
 void printGraph(int** graph, int size) {
     for (int i = 0; i < size; i++) {
-        std::cout << i+1 << " <- " << *graph[i] << std::endl;
+        if (graph[i] != NULL)
+            std::cout << i+1 << " <- " << graph[i][0] << " " << graph[i][1] << std::endl;
     }  
 }
