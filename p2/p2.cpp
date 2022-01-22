d6f3cf29fe32a5d3073246674db658f373340e6a
Valentim Santos <75221226+ValentimSts@users.noreply.github.com>
2022-01-22 01:44:30 +0000

    Update p2.cpp

--- p2/p2.cpp
+++ p2/p2.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
-#include <algorithm>
+#include <vector>
 
-typedef enum {
-    WHITE, YELLOW, RED
-} visit_color;
+#define parent1 0
+#define parent2 1
+#define n_parents 2
+#define weight 3
+#define visited 4
 
 void computeInput();
 void printGraph(int** graph, int size);
+void DFS(int** graph, int target_node, int curr_weight);
+void getLCA(int** graph, int size);
 
 int main() {
     computeInput();
@@ -62,15 +66,17 @@ void computeInput() {
             //   index 1: second parent node (if needed)
             //   index 2: number of parents
             //   index 3: weight of the node (to be found after the DFS)
-            graph[v-1] = new int[4];
+            //   index 4: number of times the node has been visited (DFS)
+            graph[v-1] = new int[5];
 
-            graph[v-1][0] = -1;
-            graph[v-1][1] = -1;
-            graph[v-1][2] = 0;
-            graph[v-1][3] = 0;
+            graph[v-1][parent1] = -1;
+            graph[v-1][parent2] = -1;
+            graph[v-1][n_parents] = 0;
+            graph[v-1][weight] = 0;
+            graph[v-1][visited] = 0;
         }
 
-        int parent_pos = graph[v-1][2];
+        int parent_pos = graph[v-1][n_parents];
 
         // If the node has more than 2 parents, the tree is invalid.
         if (parent_pos == 2) {
@@ -81,17 +87,75 @@ void computeInput() {
         graph[v-1][parent_pos] = u;
 
         // Increments the number of parents of the node
-        graph[v-1][2]++;
+        graph[v-1][n_parents]++;
     }
 
     printGraph(graph, n_vertices);
+
+    DFS(graph, v1, 0);
+    DFS(graph, v2, 0);
+
+    getLCA(graph, n_vertices);
 }
 
-void 
+
+void DFS(int** graph, int target_node, int curr_weight) {
+    // node has no parents 
+    if (graph[target_node] == NULL) {
+        graph[target_node] = new int[5];
+
+        graph[target_node][weight] = curr_weight;
+        graph[target_node][visited]++;
+
+        return;
+    }
+
+    graph[target_node][weight] = curr_weight++;
+    graph[target_node][visited]++;
+
+    for (int i = 0; i < graph[target_node][n_parents]; i++) {
+        DFS(graph, graph[target_node][i], curr_weight);
+    }
+}
+
+
+void getLCA(int** graph, int size) {
+    std::vector<int> common_nodes;
+    std::vector<int> res;
+    bool first_common = true;
+    int min;
+
+    for (int i = 0; i < size; i++) {
+        if (graph[i][visited] == 2) {
+            if (first_common) {
+                first_common = false;
+                min = graph[i][weight];
+            }
+
+            common_nodes.push_back(i);
+
+            if (graph[i][weight] < min) {
+                min = graph[i][weight];
+            }
+        }
+    }
+
+    for (auto val: common_nodes) {
+        if (graph[val][weight] == min)
+            res.push_back(val);
+    }
+
+    for (auto val: res) {
+        std::cout << val << " ";
+    }
+
+    std::cout << std::endl;
+}
+
 
 void printGraph(int** graph, int size) {
     for (int i = 0; i < size; i++) {
         if (graph[i] != NULL)
-            std::cout << i+1 << " <- " << graph[i][0] << " " << graph[i][1] << std::endl;
+            std::cout << i+1 << " <- " << graph[i][parent1] << " " << graph[i][parent2] << std::endl;
     }  
 }
